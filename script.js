8ddffad6da4069af2a606e3cd3e121953ce18606
parth178 <parth.xlro8@gmail.com>
2019-05-14 12:27:11 +0530

    new branch testing

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // alert('Hello World');
-let demo = "Hello World!"
-alert(demo);
+// let demo = "Hello World!"
+// alert(demo);
+// let admin = "Jhon"
+// let name = "admin"
+// alert(admin);
+// alert(name);
+const BIRTHDAY = '18.04.1982'; // make uppercase?
+alert(BIRTHDAY);
