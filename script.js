ca4fe632f81e11c7dc301b223c67fd523c29f331
parth178 <parth.xlro8@gmail.com>
2019-05-14 19:01:38 +0530

    complted till switch statment

--- script.js
+++ script.js
@@ -5,5 +5,35 @@
 // let name = "admin"
 // alert(admin);
 // alert(name);
-const BIRTHDAY = '18.04.1982'; // make uppercase?
-alert(BIRTHDAY);
+// const BIRTHDAY = '18.04.1982'; // make uppercase?
+// alert(BIRTHDAY);
+// alert( `the result is ${25 * 5}` );
+// alert( `Hello, ${BIRTHDAY}!` );
+// let name = "Ilya";
+
+// alert( `hello ${1}` ); // ?
+
+// alert( `hello ${"name"}` ); // ?
+
+// alert( `hello ${name}` ); // ?
+
+// alert( Number("   123   ") ); // 123
+// alert( Number("123z") );      // NaN (error reading a number at "z")
+// alert( Number(true) );        // 1
+// alert( Number(false) );       // 0
+// let age = prompt('How old are you?', 100);
+// alert(`You are ${age} years old!`);
+
+// let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+// if (year == 2015) { alert( 'You are right!' );
+// }else { alert('your wrong'); }
+
+// let i = 0;
+// while (i < 5) { // shows 0, then 1, then 2
+//   alert( i );
+//   i++;
+// }
+function showMessage() {
+    alert( 'Hello everyone!' );
+  }
